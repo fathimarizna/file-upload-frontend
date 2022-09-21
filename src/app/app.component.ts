eb97ba6168d3e66e3a6df8b1e55d6ced92738cdd
Fathima_Rizna <riziqbal97@gmail.com>
2022-09-21 12:54:19 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularUpload';
+
+  displaySingleImage!: Boolean;
+  displaySingleImageArray!: Array<any>;
+
+  @ViewChild('singleInput', { static: false })
+  singleInput!: ElementRef;
+
+  images: any;
+
+  constructor(private http: HttpClient) {
+    this.displaySingleImage = false;
+    this.displaySingleImageArray = [];
+  }
+  selectImage(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      console.log(file);
+      this.images = file;
+    }
+  }
+  onSubmit() {
+    const formdata = new FormData();
+    formdata.append('file', this.images);
+
+    this.http.post<any>('http://localhost:3000/file', formdata).subscribe((res) => {
+      console.log(res);
+      this.singleInput.nativeElement.value = '';
+      this.displaySingleImage = true;
+      this.displaySingleImageArray.push(res.path);
+    },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
